5b072ce9995642cdb539108ecf7069e1147db18a
alexlukianov <lukyanovalexwind@gmail.com>
2022-09-19 15:51:00 +0300

    Working solution.

---
+++ Logger_w_Backups/Models/Logger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Logger_w_Backups
+{
+    internal class Logger
+    {
+        public delegate void EventHandler(int iteration, List<string> logs, string log);
+        public event EventHandler DoBackup;
+
+        string ConfigPath = @"D:\Projects\C# learning\Code\Logger_w_Backups_M3_P4\Logger_w_Backups\Configs\config.json";
+
+
+        public Logger()
+        {
+            DoBackup += MakeBackup;
+        }
+
+        public void EventMethod(int iteration, List<string> logs, string log)
+        {
+            DoBackup(iteration, logs, log);
+        }
+
+        private void MakeBackup(int iteration, List<string> logs, string log)
+        {
+            var backupAt = DoBackupAt(ConfigPath);
+            if (iteration % backupAt == 0)
+            {
+                Console.WriteLine("Backup!");
+                ListToFile(logs, iteration, log);
+            }
+        }
+
+
+        private int DoBackupAt(string configPath)
+        {
+            // Reads backup condition from json file
+            string json = File.ReadAllText(ConfigPath);
+            var logConfig = JsonConvert.DeserializeObject<ConfigService>(json);
+            int backupAt = logConfig.BackupAt;
+            return backupAt;
+        }
+
+        public void ListToFile(List<string> logs, int iteration, string log)
+        {
+            var dateTime = DateTime.UtcNow;
+            var fileName = TimeToName(dateTime);
+
+            string backupPath = @$"D:\Projects\C# learning\Code\Logger_w_Backups_M3_P4\Logger_w_Backups\Backups\{iteration}{log}-{fileName}.txt";
+            File.WriteAllLines(backupPath, logs);
+
+
+            string TimeToName(DateTime time)
+            {                
+                var mils = time.Millisecond.ToString();
+                var fileName = time.ToString() + mils;
+                char[] analyse = fileName.ToCharArray();
+                for (int i = 0; i < analyse.Length; i++)
+                {
+                    if (Char.IsDigit(analyse[i]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        analyse[i] = '_';
+                    }
+                }
+                fileName = new string(analyse);
+
+                Console.WriteLine(fileName);
+                return fileName;
+                
+            }
+        }
+    }
+}
---
+++ Logger_w_Backups/Models/Starter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger_w_Backups
+{
+    internal class Starter
+    {
+        public List<string> Logs = new List<string>();
+        Logger Logger = new Logger();
+
+        public int Iteration = 0;
+
+        public void Run()
+        {
+            Console.WriteLine("The program is running");
+            Console.WriteLine("----------------------");
+
+            Task.WaitAll(WriteLogA(), WriteLogB());
+
+        }
+
+        public async Task WriteLogA()
+        {
+            await Task.Run(() => WriteLog("Log A"));
+        }
+
+        public async Task WriteLogB()
+        {
+            await Task.Run(() => WriteLog("Log B"));
+        }
+
+        public void WriteLog(string logName)
+        {
+                for (int i = 0; i < 50; i++)
+                {
+                    AddLog();
+                }
+
+                void AddLog()
+                {
+                    Iteration++;
+                    var line = LogLine(Iteration, logName);
+                    Console.WriteLine(line);
+                    Logs.Add(line);
+                    Logger.EventMethod(Iteration, Logs, logName);
+                }
+
+                string LogLine(int iter, string log)
+                {
+                    var logString = $"Iteration {iter} - {log}";
+                    return logString;
+                }            
+        }
+    }
+}
